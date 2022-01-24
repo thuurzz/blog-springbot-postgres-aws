d18aabd36a99406c7b21562e2ca7a12bcbdd68f6
Arthur Vinicius <thuur.vss@gmail.com>
2022-01-24 12:52:12 -0300

    Criação do Model e Repository

---
+++ codeblog/src/main/java/com/spring/codeblog/model/Post.java
@@ -0,0 +1,30 @@
+package com.spring.codeblog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="TB_POST")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String autor;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy")
+    private LocalDate data;
+
+    @NotBlank
+    @Lob
+    private String texto;
+
+}
