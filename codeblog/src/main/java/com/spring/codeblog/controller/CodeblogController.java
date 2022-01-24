79592af9778ba2882336fc318c7ee2732b192e04
Arthur Vinicius <thuur.vss@gmail.com>
2022-01-24 18:49:18 -0300

    Salvando um novo Post

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,11 +4,16 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.naming.Binding;
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -33,6 +38,21 @@ public class CodeblogController {
         return mv;
     }
 
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+    }
 
 
 }
